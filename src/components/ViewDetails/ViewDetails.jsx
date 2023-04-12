b69fc3b9943c38bc034497df43e6fdd03bc12ab2
Bajlar01~ <bajlarrahman24@gmail.com>
2023-04-12 10:46:43 +0600

    job details card icon setup

--- src/components/ViewDetails/ViewDetails.jsx
+++ src/components/ViewDetails/ViewDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import { CalendarDaysIcon, CurrencyDollarIcon, PhoneIcon, EnvelopeIcon, MapPinIcon } from '@heroicons/react/24/solid'
 
 const ViewDetails = () => {
   const details = useLoaderData();
@@ -13,8 +14,8 @@ const ViewDetails = () => {
   // console.log(detail);
  
   return (
-    <div className='w-10/12 mx-auto'>
-      <div className="flex gap-8 mt-5">
+    <div className='w-10/12 mx-auto mt-8'>
+      <div className="grid md:flex gap-8 mt-5">
         <div className="w-3/5 job-details">
           <p className='mb-4'><strong>Job Description:</strong> {detail.description}</p>
           <p className='mb-4'><strong>Job Responsibility:</strong> {detail.responsibility}</p>
@@ -22,17 +23,36 @@ const ViewDetails = () => {
           <p className='mb-4'><strong>Experiences:</strong> {detail.experiences}</p>
         </div>
 
-        <div className="contact-info card w-96 bg-base-100 shadow-xl">
+        <div className="contact-info card w-96 bg-base-100 border-2">
           <div className="card-body">
-            <h3 className='text-2xl text-center'>Job Details</h3>
+            <h3 className='text-2xl font-bold'>Job Details</h3>
             <hr />
-            <p className='mb-2'><strong>Salary:</strong> {detail.salary}</p>
-            <p className='mb-2'><strong>Job Title:</strong> {detail.job_title}</p>
-            <h3 className='text-2xl text-center'>Contact Information</h3>
+            <p className='mb-2 flex gap-2'> 
+              <span><CurrencyDollarIcon className="h-6 w-6" /></span> 
+              <strong>Salary:</strong> {detail.salary}
+            </p>
+
+            <p className='mb-2 flex gap-2'>
+              <span><CalendarDaysIcon className="h-6 w-6" /></span> 
+              <strong>Job Title:</strong> {detail.job_title}
+            </p>
+
+            <h3 className='text-2xl'>Contact Information</h3>
             <hr />
-            <p className='mb-2'><strong>Phone:</strong> {detail.phone}</p>
-            <p className='mb-2'><strong>Email:</strong> {detail.email}</p>
-            <p className='mb-2'><strong>Address:</strong> {detail.address}</p>
+            <p className='mb-2 flex gap-2'>
+              <span><PhoneIcon className="h-6 w-6" /></span> 
+              <strong>Phone:</strong> {detail.phone}
+            </p>
+
+            <p className='mb-2 flex gap-2'>
+              <span><EnvelopeIcon className="h-6 w-6" /></span>
+              <strong>Email:</strong> {detail.email}
+            </p>
+
+            <p className='mb-2 flex gap-2'>
+              <span><MapPinIcon className="h-6 w-6" /></span>
+              <strong>Address:</strong> {detail.address}
+            </p>
             <button className="btn btn-primary mt-5">Apply Now</button>
           </div>
         </div>
